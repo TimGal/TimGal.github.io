04ab6f2931c2700bb10b7ee68680cbc8adbc5450
Timur Galdin <galdin.timur508@gmail.com>
2020-01-13 19:55:53 +0300

    slide1

--- js/custom.js
+++ js/custom.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
   $(function() {
     $('body').vegas({
         slides: [
-            { src: 'images/slide-1.jpg' },
+            // { src: 'images/slide-1.jpg' },
             { src: 'images/slide-2.jpg' },
 			  	{ src: 'images/slide-3.jpg' }
         ],
